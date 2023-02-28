12b933a00b831766867c841530a17b3318c4d9e6
EricFithian <ef2413@columbia.edu>
2023-02-28 13:49:20 -0500

    Added my server comments

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+// Express is needed
 const express = require('express');
+// app will be what I use for express functionality in the server
 const app = express();
-const dogController = require('./controllers/dogs')
+// I am bringing in my dog controller
+const dogController = require('./controllers/dogs');
 
-// Set up middleware
+// Set up middleware so that I can have EJS views that expects a directory I call views to contain ejs
 app.set('view engine', 'ejs');
 
+// Sets up that I can use my dog controller routes AND that they're above other routes so will be read first
 app.use('', dogController);
 
+// Rest of my generic routes. Maybe a home route, a wildcard for annoying users and one or two others
 app.get('/hi', function (req, res) {
     // query info from database
     // pass it to the ejs file
@@ -26,7 +31,7 @@ app.get('/*', function(req, res) {
 //     console.log(req.params);
 //     res.send(`<h1>My first paremeter is ${req.params.params} and my second paremeter is ${req.params.secondParam}`)
 // });
-console.log("Joshua Pent was here, and he enjoys screwing up your code. >:)")
+
 
 app.listen(4000, function () {
     console.log(`I am listening on port 4000`);
